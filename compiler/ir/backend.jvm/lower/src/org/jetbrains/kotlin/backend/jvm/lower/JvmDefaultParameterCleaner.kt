9700c01a878ae2f169455815bf30d42b005d4486
Alexander Udalov <alexander.udalov@jetbrains.com>
2024-02-22 00:21:35 +0100

    IR: introduce LoweringPhase, use it for JVM file phases

--- compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/JvmDefaultParameterCleaner.kt
+++ compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/JvmDefaultParameterCleaner.kt
@@ -6,8 +6,14 @@
 package org.jetbrains.kotlin.backend.jvm.lower
 
 import org.jetbrains.kotlin.backend.common.lower.DefaultParameterCleaner
+import org.jetbrains.kotlin.backend.common.phaser.LoweringPhase
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 
+@LoweringPhase(
+    name = "DefaultParameterCleaner",
+    description = "Replace default values arguments with stubs",
+    prerequisite = [JvmDefaultArgumentStubGenerator::class]
+)
 internal class JvmDefaultParameterCleaner(
     context: JvmBackendContext
 ) : DefaultParameterCleaner(context, replaceDefaultValuesWithStubs = true)
