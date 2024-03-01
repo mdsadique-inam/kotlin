37b86628df3e895f40381b2778808395be278812
Igor Chevdar <igor.chevdar@jetbrains.com>
2024-03-01 10:50:12 +0200

    [tests] Added some tests on generic safe casts

---
+++ compiler/testData/codegen/box/basics/unchecked_cast12.kt
@@ -0,0 +1,10 @@
+// IGNORE_BACKEND: JS_IR, JS_IR_ES6
+
+fun box(): String {
+    try {
+        val x = ("1" as Comparable<Any>).compareTo(2)
+        return "FAIL: $x"
+    } catch (e: ClassCastException) {
+        return "OK"
+    }
+}
