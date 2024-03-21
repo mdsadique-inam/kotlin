1b3f28514c9a500a6c3c76ab0d74d1386b1fdf1e
Alexander Udalov <alexander.udalov@jetbrains.com>
2024-03-22 00:25:55 +0100

    JVM IR: use LoweringPhase for module-level lowering phases

--- compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/JvmIrInliner.kt
+++ compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/JvmIrInliner.kt
@@ -5,10 +5,16 @@
 
 package org.jetbrains.kotlin.backend.jvm.lower
 
+import org.jetbrains.kotlin.backend.common.phaser.LoweringPhase
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.inline.FunctionInlining
 
+@LoweringPhase(
+    name = "FunctionInliningPhase",
+    description = "Perform function inlining",
+    prerequisite = [JvmExpectDeclarationRemover::class]
+)
 class JvmIrInliner(context: JvmBackendContext) : FunctionInlining(
     context,
     innerClassesSupport = context.innerClassesSupport,
