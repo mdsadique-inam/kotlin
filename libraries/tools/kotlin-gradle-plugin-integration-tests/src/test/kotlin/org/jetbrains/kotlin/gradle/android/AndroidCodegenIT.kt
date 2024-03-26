8ec1e1f784f63a524e3172a778972de7a67334de
Iaroslav Postovalov <iaroslav.postovalov@jetbrains.com>
2024-03-26 13:55:02 +0100

    Linux

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/android/AndroidCodegenIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/android/AndroidCodegenIT.kt
@@ -8,18 +8,37 @@ package org.jetbrains.kotlin.gradle.android
 import org.gradle.api.logging.LogLevel
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.testbase.*
+import org.jetbrains.kotlin.test.util.KtTestUtil
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.condition.OS
 
+/**
+ * This function patches the Android emulator to always run without acceleration (-no-accel).
+ * This is a necessary hack due to the inability to pass the -no-accel argument directly to a Gradle-managed emulator run.
+ * It allows builds to proceed on servers that do not support nested virtualization.
+ *
+ * Workaround of https://issuetracker.google.com/issues/241029220
+ */
+private fun patchEmulatorAlwaysNoAccel() {
+    val emulatorDir = KtTestUtil.findAndroidSdk().resolve("emulator")
+    val emulatorFile = emulatorDir.resolve("emulator")
+    val emulator1File = emulatorDir.resolve("emulator1")
+    if (!emulator1File.exists()) {
+        emulatorFile.renameTo(emulator1File)
+        emulatorFile.writeText(
+            """
+            |#!/bin/sh
+            |DIR="$(dirname "$(readlink -f "$0")")"
+            |"${'$'}DIR/emulator1" -no-accel "$@"
+            """.trimMargin()
+        )
+        emulatorFile.setExecutable(true)
+    }
+}
+
 @DisplayName("codegen tests on android")
 @AndroidTestVersions(minVersion = TestVersions.AGP.AGP_82, maxVersion = TestVersions.AGP.AGP_82)
-/*
- * Non-virtualized runners are necessary.
- * This build utilizes the Android Emulator to execute codegen tests,
- * requiring a VM hypervisor for optimal performance.
- * Nested virtualization on CI runners except physical Macs fails.
- */
-@OsCondition(enabledOnCI = [OS.MAC])
+@OsCondition(supportedOn = [OS.MAC, OS.LINUX], enabledOnCI = [OS.MAC, OS.LINUX])
 @AndroidCodegenTests
 class AndroidCodegenIT : KGPBaseTest() {
     @GradleAndroidTest
@@ -33,14 +52,34 @@ class AndroidCodegenIT : KGPBaseTest() {
             gradleVersion,
             buildOptions = defaultBuildOptions.copy(
                 androidVersion = agpVersion,
-                freeArgs = listOf("-Pandroid.useAndroidX=true", "-Pandroid.experimental.testOptions.managedDevices.setupTimeoutMinutes=0"),
-                logLevel = LogLevel.LIFECYCLE
+                freeArgs = listOf(
+                    "-Pandroid.useAndroidX=true",
+                    "-Pandroid.experimental.testOptions.managedDevices.setupTimeoutMinutes=0"
+                ),
+                logLevel = LogLevel.INFO
             ),
             enableGradleDebug = false,
             buildJdk = jdkVersion.location
         ) {
             build("assembleAndroidTest", forceOutput = true, enableGradleDaemonMemoryLimitInMb = 6000)
-            build("nexusCheck", forceOutput = true, enableGradleDaemonMemoryLimitInMb = 6000)
+            var nonAcceleratedEmulator = false
+            try {
+                build("nexusSetup", forceOutput = true, enableGradleDaemonMemoryLimitInMb = 6000)
+            } catch (e: org.gradle.testkit.runner.UnexpectedBuildFailure) {
+                patchEmulatorAlwaysNoAccel()
+                nonAcceleratedEmulator = true
+            }
+            build(
+                "nexusCheck",
+                buildOptions = buildOptions.copy(
+                    freeArgs = if (nonAcceleratedEmulator)
+                        buildOptions.freeArgs + "-Pandroid.testoptions.manageddevices.emulator.gpu=swiftshader_indirect" + "-PforceArmEmulator=true"
+                    else
+                        buildOptions.freeArgs
+                ),
+                forceOutput = true,
+                enableGradleDaemonMemoryLimitInMb = 6000
+            )
         }
     }
 }
