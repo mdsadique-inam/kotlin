89003b5d66813e6f90f9ef8ce916dae8753a316f
Nataliya.Valtman <nataliya.valtman@jetbrains.com>
2024-04-09 13:34:56 +0200

    Update error message when kotlin-native-prebuilt overrides native version
    native version from kotlin.native.version and native version from
    kotlin-native-prebuilt should be the same
    
    #KT-65936 Fixed

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/toolchain/KotlinNativeBundleBuildService.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/toolchain/KotlinNativeBundleBuildService.kt
@@ -195,7 +195,7 @@ internal abstract class KotlinNativeBundleBuildService : BuildService<BuildServi
             ?: error(resolutionErrorMessage)
 
         if (!gradleCachesKotlinNativeDir.exists()) {
-            throw IllegalArgumentException(resolutionErrorMessage)
+            error("Kotlin Native prebuild dependency was used. Please provide corresponding version in 'kotlin.native.version' property")
         }
         return gradleCachesKotlinNativeDir
     }
