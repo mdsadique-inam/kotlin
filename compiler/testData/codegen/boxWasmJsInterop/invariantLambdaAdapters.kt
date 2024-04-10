b77259e3e65bb8074fcd6144ac72432c9efc1106
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2024-04-10 15:41:25 +0200

    AMEND

--- compiler/testData/codegen/boxWasmJsInterop/invariantLambdaAdapters.kt
+++ compiler/testData/codegen/boxWasmJsInterop/invariantLambdaAdapters.kt
@@ -1,28 +1,31 @@
 // TARGET_BACKEND: WASM
-// IGNORE_BACKEND: WASM
 // Ignore reason KT-xxxxx
 
 fun f1(x: Number): String = x.toString()
 fun f2(x: Number): String = x.toString()
 
-fun floatLambda(f: (Float) -> String): String = js("f(42.2)")
-fun byteLambda(f: (Byte) -> String): String =  js("f(42)")
+fun floatLambda(f: (Float) -> String): String = js("f(2.5)")
+fun byteLambda(f: (Byte) -> String): String =  js("f(2.5)")
 
 fun box(): String {
 
     val f1ref = ::f1
     val resultAsFloat1 = floatLambda(f1ref)
+    assertEquals(resultAsFloat1, "2.5")
     val resultAsByte1 = byteLambda(f1ref)
+    assertEquals(resultAsByte1, "2")
 
     val f2ref = ::f2
     val resultAsByte2 = byteLambda(f2ref)
+    assertEquals(resultAsByte2, "2")
     val resultAsFloat2 = floatLambda(f2ref)
+    assertEquals(resultAsFloat2, "2.5")
 
-    if (resultAsFloat1 != resultAsFloat2) return "FAIL1"
-    if (resultAsByte1 != resultAsByte2) return "FAIL2"
+    assertEquals(resultAsFloat1, resultAsFloat2)
+    assertEquals(resultAsByte1, resultAsByte2)
 
-    if (resultAsFloat1 == resultAsByte1) return "FAIL3"
-    if (resultAsFloat2 == resultAsByte2) return "FAIL4"
+    assertNotEquals(resultAsFloat1, resultAsByte1)
+    assertNotEquals(resultAsFloat2, resultAsByte2)
 
     return "OK"
 }
