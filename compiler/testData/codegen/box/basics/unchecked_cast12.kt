4c14c72ad8d7d6eb7f8ab3ee61a240c1ae3f256f
Igor Chevdar <igor.chevdar@jetbrains.com>
2024-04-10 16:04:26 +0300

    fixup! [tests] Added some tests on generic safe casts

--- compiler/testData/codegen/box/basics/unchecked_cast12.kt
+++ compiler/testData/codegen/box/basics/unchecked_cast12.kt
@@ -1,9 +1,15 @@
 // FREE_COMPILER_ARGS: -Xbinary=genericSafeCasts=true
 // IGNORE_BACKEND: JS_IR, JS_IR_ES6
 
+class Foo(val s: String): Comparable<Foo> {
+    override fun compareTo(other: Foo): Int {
+        return s.compareTo(other.s)
+    }
+}
+
 fun box(): String {
     try {
-        val x = ("1" as Comparable<Any>).compareTo(2)
+        val x = (Foo("1") as Comparable<Any>).compareTo(2)
         return "FAIL: $x"
     } catch (e: ClassCastException) {
         return "OK"
