d012746486b79771c0d22e9e2106cc3c57cb134a
Igor Chevdar <igor.chevdar@jetbrains.com>
2024-04-10 16:21:11 +0300

    fixup! [K/N][tests] Added couple of file-check tests on bridges

--- native/native.tests/testData/codegen/fileCheck/bridges.kt
+++ native/native.tests/testData/codegen/fileCheck/bridges.kt
@@ -23,6 +23,18 @@ class Bar2 : Foo2<Unit>(Unit), I2
 
 fun getX2(i: I2) = i.x
 
+interface I3 {
+    fun foo(): Nothing
+}
+
+open class Foo3<T : Int> {
+    fun foo(): T = TODO()
+}
+
+class Bar3 : Foo3<Nothing>(), I3
+// CHECK-LABEL: define void @"kfun:Bar3#$<bridge-DNN>foo(){}kotlin.Nothing(){}kotlin.Nothing
+// CHECK-LABEL: epilogue:
+
 // CHECK-LABEL: define %struct.ObjHeader* @"kfun:#box(){}kotlin.String"
 fun box(): String {
     val i1: I1 = C1()
@@ -32,6 +44,11 @@ fun box(): String {
 
     val i2: I2 = Bar2()
     getX2(i2)
+
+    val i3: I3 = Bar3()
+    try {
+        i3.foo()
+    } catch (t: Throwable) { }
 
     return "OK"
 }
