742c0728e90c04609c6854a493338700fccb8c31
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2024-04-11 23:14:53 +0200

    [AA] Remove expanded type rendering from class and function type renderers
    - `KtTypeRenderer` should handle abbreviated/expanded type rendering
      exclusively, so that this functionality is centralized and governed by
      a single option rather than scattered all over the place.
    - It should also be noted that not only class types may have abbreviated
      types (now or in the future). For example, function types can be
      expanded from a type alias, although `abbreviatedType` currently does
      not properly support function types.
    
    ^KT-62889

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/renderer/types/renderers/KtUsualClassTypeRenderer.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/renderer/types/renderers/KtUsualClassTypeRenderer.kt
@@ -7,7 +7,6 @@ package org.jetbrains.kotlin.analysis.api.renderer.types.renderers
 
 import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
 import org.jetbrains.kotlin.analysis.api.renderer.types.KtTypeRenderer
-import org.jetbrains.kotlin.analysis.api.symbols.KtTypeAliasSymbol
 import org.jetbrains.kotlin.analysis.api.types.KtTypeNullability
 import org.jetbrains.kotlin.analysis.api.types.KtUsualClassType
 import org.jetbrains.kotlin.analysis.utils.printer.PrettyPrinter
@@ -29,37 +28,6 @@ public interface KtUsualClassTypeRenderer {
                     }
                 },
             )
-        }
-    }
-
-    /**
-     * Renders class type and in case of type alias adds a comment containing fully expanded class type, for example:
-     * ```
-     * typealias MyInt = Int
-     * ```
-     * `MyInt` is rendered as `MyInt /* = Int */`
-     */
-    public object AS_CLASS_TYPE_WITH_TYPE_ARGUMENTS_VERBOSE : KtUsualClassTypeRenderer {
-        context(KtAnalysisSession, KtTypeRenderer)
-        override fun renderType(type: KtUsualClassType, printer: PrettyPrinter): Unit = printer {
-            AS_CLASS_TYPE_WITH_TYPE_ARGUMENTS.renderType(type, printer)
-            if (type.classSymbol is KtTypeAliasSymbol) {
-                append(" /* = ")
-                renderTypeIgnoringAbbreviation(type.fullyExpandedType, printer)
-                append(" */")
-            }
-        }
-    }
-
-    public object AS_FULLY_EXPANDED_CLASS_TYPE_WITH_TYPE_ARGUMENTS : KtUsualClassTypeRenderer {
-        context(KtAnalysisSession, KtTypeRenderer)
-        override fun renderType(type: KtUsualClassType, printer: PrettyPrinter) {
-            val fullyExpandedType = type.fullyExpandedType
-            if (fullyExpandedType is KtUsualClassType) {
-                AS_CLASS_TYPE_WITH_TYPE_ARGUMENTS.renderType(fullyExpandedType, printer)
-            } else {
-                renderType(fullyExpandedType, printer)
-            }
         }
     }
 }
