04bdb8526c1b377cc8e23520d0751a703e4a8db0
Gleb Lukianets <gleb.lukianets@jetbrains.com>
2024-04-12 17:42:04 +0200

    fixup! Rebase

--- native/swift/swift-export-standalone/testData/typealiases/golden_result/result.swift
+++ native/swift/swift-export-standalone/testData/typealiases/golden_result/result.swift
@@ -4,9 +4,16 @@ import KotlinRuntime
 public typealias RegularInteger = Swift.Int32
 public extension main.typealiases.inner {
     public typealias LargeInteger = Swift.Int64
+    public typealias Foo = main.typealiases.Foo
+    public class Bar {
+        public init() {
+            fatalError()
+        }
+    }
 }
 public extension main.typealiases {
     public typealias SmallInteger = Swift.Int16
+    public typealias Bar = main.typealiases.inner.Bar
     public class Foo {
         public init() {
             fatalError()
