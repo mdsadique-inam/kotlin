41ec8c668f8110eb7d5f95fa0a1ccbd686ef50eb
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2024-04-12 11:04:34 +0200

    fixup! [JS] Add test for KT-57192

--- compiler/testData/diagnostics/testsWithJsStdLib/export/promiseUnit.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/export/promiseUnit.kt
@@ -1,12 +1,15 @@
 // FIR_IDENTICAL
 // ISSUE: KT-57192
-// WITH_COROUTINES
 @file:OptIn(ExperimentalJsExport::class)
 import kotlin.js.Promise
 
 @JsExport
-fun fooInt(p: Promise<Int>): Promise<Int> = p
+fun fooInt(p: Promise<Int>): Promise<Int>? = p
 
 <!NON_EXPORTABLE_TYPE!>@JsExport
-fun fooUnit(<!NON_EXPORTABLE_TYPE!>p: Promise<Unit><!>): Promise<Unit><!> = p
+fun fooUnitReturn(): Promise<Unit>?<!> = null
 
+@JsExport
+fun fooUnitArgument(<!NON_EXPORTABLE_TYPE!>p: Promise<Unit><!>) {
+    p.then {}
+}
